7308d85a990fd24a32f5375519e270e6d27ea88a
SamuelMarinh <samuelmarinhodealmeida@gmail.com>
2023-05-10 13:55:19 -0300

    Up Analise_Titanic

--- Estatística Descritivas - Titanic/Analise_Titanic.py
+++ Estatística Descritivas - Titanic/Analise_Titanic.py
@@ -1,11 +1,10 @@
-import io
 import pandas as pd
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
 
 # Caminho do arquivo Excel
-caminho_arquivo = 'C:/Users/samuel.marinho/Desktop/Python/Analises/train.csv'
+caminho_arquivo = 'C:/Users/samuel.marinho/Desktop/Python/Estatistica_descritiva_Python/Estatística Descritivas - Titanic/train.csv'
 
 # Leitura do arquivo em Excel
 dados = pd.read_csv(caminho_arquivo, sep=',')
@@ -23,7 +22,7 @@ print(f'\nAnalise das 3 primeiras linhas:\n\n{dh}')
 dt = dados.tail(3)
 print(f'\n\nAnalise das 3 ultimas linhas:\n\n{dt}')
 
-# Estatistica geral com os principais calculos usados
+# Estatistica geral com os principais calculos usados: Contagem; media; desvio padrao; minimo; primeiro; segundo e terceiro quartil; valor max.
 ddes = dados.describe()
 print(f'\n\nEstatistica geral:\n\n{ddes}')
 
@@ -48,5 +47,29 @@ dados.plot(kind = "scatter", x = "Fare", y = "Survived", color = "r", linewidth 
 plt.show()
 
 # Sobreviventes em relação a classe do navio (titanic)
-sns.barplot(x = "Classe", y = "Sobreviventes", data = dados)
+sns.barplot(x = "Pclass", y = "Survived", data = dados)
+plt.show()
+
+# Sobreviventes em realção ao sexo
+sns.barplot(x = "Sex", y = "Survived", data = dados)
+plt.show()
+
+# Informações de Idade e Sexo
+survived = 'survived'
+not_survived = 'not survived'
+
+fig, axes = plt.subplots(nrows=1, ncols=2, figsize=(10, 4))
+women = dados[dados['Sex'] == 'female']
+men = dados[dados['Sex'] == 'male']
+
+ax = sns.histplot(women[women['Survived'] == 1].Age.dropna(), bins=20, label=survived, ax=axes[0], kde=False)
+ax = sns.histplot(women[women['Survived'] == 0].Age.dropna(), bins=40, label=not_survived, ax=axes[0], kde=False)
+ax.legend()
+ax.set_title('Female')
+
+ax = sns.histplot(men[men['Survived'] == 1].Age.dropna(), bins=20, label=survived, ax=axes[1], kde=False)
+ax = sns.histplot(men[men['Survived'] == 0].Age.dropna(), bins=40, label=not_survived, ax=axes[1], kde=False)
+ax.legend()
+ax.set_title('Male')
+
 plt.show()
